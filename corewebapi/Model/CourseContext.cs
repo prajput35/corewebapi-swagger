1decfabcc78df47fdb8b79e19f880b9fdabc728c
Paras <Paras.code.api@gmail.com>
2020-06-17 03:41:49 +0530

    Add project files.

---
+++ corewebapi/Model/CourseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace corewebapi.Model
+{
+    public class CourseContext : DbContext
+    {
+        // Replace your connecting string
+        readonly string connectionstring = "";
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(connectionstring);
+            base.OnConfiguring(options);
+        }
+    }
+
+    
+}
